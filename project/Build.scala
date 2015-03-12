72c8a5e0a8e54de33a85edf2d35e6b12549afb14
Koert Kuipers <koert@tresata.com>
2015-03-12 13:44:46 -0400

    upgrade to kafka 0.8.2.1 and ppublish to sonatype OSSRH

--- project/Build.scala
+++ project/Build.scala
@@ -16,7 +16,7 @@ object ProjectBuild extends Build {
       scalacOptions ++= Seq("-unchecked", "-deprecation", "-target:jvm-1.6"),
       libraryDependencies ++= Seq(
         "org.slf4j" % "slf4j-api" % "1.7.5" % "compile",
-        "org.apache.kafka" %% "kafka" % "0.8.2.0" % "compile"
+        "org.apache.kafka" %% "kafka" % "0.8.2.1" % "compile"
           exclude("org.jboss.netty", "netty")
           exclude("com.sun.jmx", "jmxri")
           exclude("com.sun.jdmk", "jmxtools")
@@ -30,7 +30,36 @@ object ProjectBuild extends Build {
       ),
       publishMavenStyle := true,
       pomIncludeRepository := { x => false },
-      publishArtifact in Test := false
+      publishArtifact in Test := false,
+      publishTo := {
+        val nexus = "https://oss.sonatype.org/"
+        if (isSnapshot.value)
+          Some("snapshots" at nexus + "content/repositories/snapshots")
+        else
+          Some("releases"  at nexus + "service/local/staging/deploy/maven2")
+      },
+      credentials += Credentials(Path.userHome / ".m2" / "credentials_sonatype"),
+      pomExtra := (
+        <url>https://github.com/tresata/spark-scalding</url>
+        <licenses>
+          <license>
+            <name>Apache 2</name>
+            <url>http://www.apache.org/licenses/LICENSE-2.0.txt</url>  
+            <distribution>repo</distribution>
+            <comments>A business-friendly OSS license</comments>
+          </license>
+        </licenses>
+        <scm>
+          <url>git@github.com:tresata/spark-scalding.git</url>
+          <connection>scm:git:git@github.com:tresata/spark-scalding.git</connection>
+        </scm>
+        <developers>
+          <developer>
+            <id>koertkuipers</id>
+            <name>Koert Kuipers</name>
+            <url>https://github.com/koertkuipers</url>
+          </developer>
+        </developers>)
     )
   )
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,1 +1,3 @@
 addSbtPlugin("net.virtual-void" % "sbt-dependency-graph" % "0.7.4")
+
+addSbtPlugin("com.jsuereth" % "sbt-pgp" % "1.0.0")
